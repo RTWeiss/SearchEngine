f0b3f660825a41d36bf41231330699de882c1e13
RTWeiss <ricky@rtweiss.com>
2023-07-18 10:42:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ from datetime import datetime
 
 DATABASE_URL = os.getenv('DATABASE_URL')
 
-if DATABASE_URL.startswith("postgres://"):
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 app = Flask(__name__)
