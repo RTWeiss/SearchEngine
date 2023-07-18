e0ab5685bd950dc9a2bce21587b48cb4deeda762
RTWeiss <ricky@rtweiss.com>
2023-07-18 09:35:37 -0400

    optimized app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import pickle
-from flask import Flask, render_template, request, redirect, url_for, flash, current_app
+from flask import Flask, render_template, request, redirect, url_for, flash
 from bs4 import BeautifulSoup
 import requests
 import threading
@@ -13,7 +13,6 @@ from urllib.parse import urljoin, urlparse
 import urllib.parse
 import random
 from models import db, IndexedURL
-import psycopg2
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
@@ -21,11 +20,8 @@ app.secret_key = 'your_secret_key'
 lock = Lock()
 INDEX = {}
 SITEMAP_STATUS = {}
-TOTAL_INDEXED_PAGES = 0  # variable to keep track of the total number of pages indexed
-TOTAL_SEARCHES = 0  # variable to keep track of the total number of searches
-SEARCH_QUERIES = {}  # dictionary to keep track of each search query along with its frequency
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://objskwxzxzuvdd:a7c3fa0a58658cb7b21dc6dae288945d6553533a378259af35abd16d707beb09@ec2-34-226-11-94.compute-1.amazonaws.com:5432/dbsdd3r2uu4alq'  # Update with your Heroku Postgres connection details
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://objskwxzxzuvdd:a7c3fa0a58658cb7b21dc6dae288945d6553533a378259af35abd16d707beb09@ec2-34-226-11-94.compute-1.amazonaws.com:5432/dbsdd3r2uu4alq'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
@@ -33,77 +29,18 @@ SITEMAP_QUEUE = queue.Queue()
 MAX_SIMULTANEOUS_INDEXING = 5
 CURRENTLY_INDEXING = 0
 
-def calculate_relevance_score(query, data):
-    # Implement your own function to calculate the relevance score based on the search query and data for a URL
-    # You can consider factors like the presence and frequency of query terms in the title, description, and content of the page
-    # Assign a numerical score representing the relevance of the page to the search query
-    # Higher scores indicate higher relevance
-
-    title = data['title'].lower()
-    description = data['description'].lower()
-
-    # Example: Relevance scoring based on the presence of query terms in the title and description
-    score = 0
-    title_matches = title.count(query)
-    description_matches = description.count(query)
-
-    # Increase the score for multiple instances of the search query in the title or description
-    score += title_matches
-    score += description_matches
-
-    # Add additional points for an exact match of the search query
-    if query in title:
-        score += 5
-    if query in description:
-        score += 3
-
-    return score
-
 @app.route('/', methods=['GET', 'POST'])
 def search():
-    global TOTAL_SEARCHES, SEARCH_QUERIES, INDEX
-
-    query = None  # Initialize query variable here
-    results = {}
-    ad = {}  # Initialize ad variable here
-
     if request.method == 'POST':
         query = request.form.get('query').lower()
-        TOTAL_SEARCHES += 1  # increment total number of searches
-        SEARCH_QUERIES[query] = SEARCH_QUERIES.get(query, 0) + 1  # increment search query frequency
 
         results = {
             url: {**data, 'title': escape(data['title']), 'description': escape(data['description'])}
             for url, data in INDEX.items()
             if query in data['title'].lower() or query in url.lower() or query in data['description'].lower()
         }
-
-        # Calculate relevance score for each result based on the search query
-        for url, data in results.items():
-            relevance_score = calculate_relevance_score(query, data)  # Implement your own function to calculate the relevance score
-            results[url]['relevance_score'] = relevance_score
-
-        # Sort the results based on relevance score
-        results = dict(sorted(results.items(), key=lambda x: x[1]['relevance_score'], reverse=True))
-
-        # Pick a random URL for the ad
-        ad_url = None
-        while ad_url is None or results[ad_url]['type'] == 'image':
-         if results:
-            ad_url = random.choice(list(results.keys()))
-            ad = {
-                'title': results[ad_url]['title'],
-                'url': ad_url,
-                'description': results[ad_url]['description']
-            }
-        else:
-            ad = {}
-
-
-    if request.method == 'POST':
-        return render_template('results.html', query=query, results=results, ad=ad)
+        return render_template('results.html', query=query, results=results)
     return render_template('search.html')
-
 
 def process_sitemap_queue():
     global CURRENTLY_INDEXING
@@ -117,77 +54,40 @@ def process_sitemap_queue():
                 CURRENTLY_INDEXING += 1
 
 def index_sitemap(sitemap_url):
-    global CURRENTLY_INDEXING, TOTAL_INDEXED_PAGES, INDEX
-    index = 0  # Variable to keep track of the index count
+    global CURRENTLY_INDEXING, INDEX
+
     SITEMAP_STATUS[sitemap_url] = {'status': 'Indexing started...'}
+    urls = get_urls_from_sitemap(sitemap_url)
+    SITEMAP_STATUS[sitemap_url]['total_urls'] = len(urls)
 
-    with app.app_context():  # Set up the application context
-        try:
-            response = requests.get(sitemap_url)
-            soup = BeautifulSoup(response.text, "html.parser")  # Use html.parser as the parser library
-            urls = [loc.text for loc in soup.find_all("loc")]
+    for url in urls:
+        if sitemap_url not in SITEMAP_STATUS:
+            print(f"Stopped indexing for deleted sitemap: {sitemap_url}")
+            return
+        else:
+            try:
+                index_url(url)
+            except Exception as e:
+                print(f"Error occurred while indexing URL {url}: {e}")
 
-            # Add the total number of URLs to SITEMAP_STATUS
-            SITEMAP_STATUS[sitemap_url]['total_urls'] = len(urls)
-            SITEMAP_STATUS[sitemap_url]['indexed_urls'] = 0
+    CURRENTLY_INDEXING -= 1
+    SITEMAP_STATUS[sitemap_url]['status'] = 'Indexing finished'
 
-            image_extensions = ['.jpg', '.jpeg', '.png', '.gif', '.svg', '.bmp', '.webp']
+    process_sitemap_queue()
 
-            for url in urls:
-                if sitemap_url not in SITEMAP_STATUS:
-                    print(f"Stopped indexing for deleted sitemap: {sitemap_url}")
-                    return
+def get_urls_from_sitemap(sitemap_url):
+    response = requests.get(sitemap_url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    urls = [loc.text for loc in soup.find_all("loc")]
+    return urls
 
-                if url.endswith('.xml'):
-                    SITEMAP_QUEUE.put(url)
-                    SITEMAP_STATUS[url] = 'Added to queue'
-                    process_sitemap_queue()
-                else:
-                    try:
-                        res = requests.get(url)
-                        page_soup = BeautifulSoup(res.text, "html.parser")
-                        title = page_soup.find("title").text if page_soup.find("title") else url
-                        description = page_soup.find("meta", attrs={"name": "description"})
-                        description = description["content"] if description else "No description available"
-
-                        url_type = "webpage"
-
-                        if any(re.search(ext, url) for ext in image_extensions):
-                            url_type = "image"
-
-                        new_data = {
-                            "title": title,
-                            "description": description,
-                            "type": url_type
-                        }
-
-                        with db.session.begin():  # Remove subtransactions=True
-                            with lock:
-                                if url in INDEX:
-                                    if INDEX[url] != new_data:
-                                        INDEX[url] = new_data
-                                        TOTAL_INDEXED_PAGES += 1  # increment total number of pages indexed
-                                        SITEMAP_STATUS[sitemap_url]['indexed_urls'] += 1
-                                else:
-                                    INDEX[url] = new_data
-                                    TOTAL_INDEXED_PAGES += 1  # increment total number of pages indexed
-                                    SITEMAP_STATUS[sitemap_url]['indexed_urls'] += 1
-
-                                indexed_url = IndexedURL(url=url, title=title, description=description, type=url_type)
-                                db.session.add(indexed_url)
-
-                        db.session.commit()
-                        index += 1  # Increment the index count for each URL processed
-
-                    except Exception as e:
-                        print(f"Error occurred while indexing URL {url}: {e}")
-
-        finally:
-            CURRENTLY_INDEXING -= 1
-            SITEMAP_STATUS[sitemap_url]['status'] = 'Indexing finished'
-            SITEMAP_STATUS[sitemap_url]['indexed_count'] = index  # Set the indexed count for the sitemap URL
-
-            process_sitemap_queue()
+def index_url(url):
+    res = requests.get(url)
+    page_soup = BeautifulSoup(res.text, "html.parser")
+    title = page_soup.find("title").text if page_soup.find("title") else url
+    description = page_soup.find("meta", attrs={"name": "description"})
+    description = description["content"] if description else "No description available"
+    INDEX[url] = {"title": title, "description": description}
 
 @app.route("/submit", methods=["GET", "POST"])
 def submit():
@@ -203,45 +103,22 @@ def submit():
 
 @app.route("/dashboard", methods=["GET"])
 def dashboard():
-    top_search_queries = dict(sorted(SEARCH_QUERIES.items(), key=lambda item: item[1], reverse=True)[:10])
-    top_search_queries_first_half = list(top_search_queries.items())[:5]
-    top_search_queries_second_half = list(top_search_queries.items())[5:]
-    return render_template("dashboard.html", 
-                           sitemap_status=SITEMAP_STATUS, 
-                           total_pages=TOTAL_INDEXED_PAGES, 
-                           total_searches=TOTAL_SEARCHES, 
-                           top_search_queries_first_half=top_search_queries_first_half, 
-                           top_search_queries_second_half=top_search_queries_second_half)
-
+    return render_template("dashboard.html", sitemap_status=SITEMAP_STATUS)
 
 @app.route("/urls", methods=["GET"])
 def urls():
-    index = IndexedURL.query.all()
-    return render_template("urls.html", index=index)
-
+    return render_template("urls.html", index=INDEX)
 
 @app.route('/delete_sitemap')
 def delete_sitemap():
     sitemap_url = request.args.get('sitemap_url')
     sitemap_url = urllib.parse.unquote(sitemap_url)
-    sitemap_entry = SITEMAP_STATUS.get(sitemap_url)
-    if sitemap_entry:
-        db.session.query(IndexedURL).filter(IndexedURL.url.startswith(sitemap_url)).delete(synchronize_session=False)
-        db.session.commit()
+    if sitemap_url in SITEMAP_STATUS:
         del SITEMAP_STATUS[sitemap_url]
-        flash('Sitemap and corresponding URLs have been deleted successfully')
+        flash('Sitemap has been deleted successfully')
     else:
         flash('Sitemap URL not found')
     return redirect(url_for('dashboard'))
 
-
-@app.route('/all_search_queries')
-def all_search_queries():
-    # Get all search queries, not just top 10
-    all_search_queries = dict(sorted(SEARCH_QUERIES.items(), key=lambda item: item[1], reverse=True))
-    return render_template('all_search_queries.html', all_search_queries=all_search_queries)
-
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
