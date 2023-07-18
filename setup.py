52a29b6e23f3e04fe813279956182ea464c6c78a
RTWeiss <ricky@rtweiss.com>
2023-07-17 21:46:53 -0400

    e

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='SearchEngine',
+    version='0.1.0',
+    packages=['your-app-package'],
+    include_package_data=True,
+    install_requires=[
+        'flask==2.0.2',
+        'bs4==0.0.1',
+        'requests==2.26.0',
+        'threading==4.5.3',
+        'pickle-mixin==1.0.2',
+        'werkzeug==2.0.2',
+    ],
+)
